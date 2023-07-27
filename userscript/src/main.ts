d3c844b0ac22eb6e3ea5eb7491f825c1017bd0ed
c915 <113941500+c915@users.noreply.github.com>
2023-07-27 02:27:24 +0200

    handle websocket 'open' and 'error' events

--- userscript/src/main.ts
+++ userscript/src/main.ts
@@ -2,20 +2,27 @@ import { OdyseeScraper } from './scrapers/OdyseeScraper';
 import { RumbleScraper } from './scrapers/RumbleScraper';
 import { YouTubeScraper } from './scrapers/YouTubeScraper';
 
-if (document.location.host === 'www.youtube.com') {
+(function () {
     const ws = new WebSocket('ws://localhost:3000/chat.ws');
-    const scraper = new YouTubeScraper(ws);
-    scraper.chatBind();
-}
 
-if (document.location.host === 'odysee.com') {
-    const ws = new WebSocket('ws://localhost:3000/chat.ws');
-    const scraper = new OdyseeScraper(ws);
-    scraper.chatBind();
-}
+    ws.addEventListener('open', (_event) => {
+        if (document.location.host === 'www.youtube.com') {
+            const scraper = new YouTubeScraper(ws);
+            scraper.chatBind();
+        }
 
-if (document.location.host === 'rumble.com') {
-    const ws = new WebSocket('ws://localhost:3000/chat.ws');
-    const scraper = new RumbleScraper(ws);
-    scraper.chatBind();
-}
+        if (document.location.host === 'odysee.com') {
+            const scraper = new OdyseeScraper(ws);
+            scraper.chatBind();
+        }
+
+        if (document.location.host === 'rumble.com') {
+            const scraper = new RumbleScraper(ws);
+            scraper.chatBind();
+        }
+    });
+
+    ws.addEventListener('error', (_event) => {
+        console.log('Could not connect to Stream-Nexus WebSocket server.');
+    });
+})();
