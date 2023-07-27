33203422c0cbafa7357c5a6985a2d1d3f79ab43d
c915 <113941500+c915@users.noreply.github.com>
2023-07-27 05:02:58 +0200

    handle chat-overlay reconnect

--- public/script.js
+++ public/script.js
@@ -1,31 +1,49 @@
-(function () {
-    const main = document.querySelector("main");
+// https://stackoverflow.com/a/23176223
+function connect(reconnected = false) {
 
-    // Create WebSocket connection.
-    let socket = new WebSocket("ws://localhost:3000/chat.ws");
-    const reconnect = (socket) => {
-        // check if socket is connected
-        if (socket.readyState === WebSocket.OPEN || socket.readyState === WebSocket.CONNECTING) {
-            return true;
+    var main = document.querySelector('main');
+    let ws = new WebSocket('ws://localhost:3000/chat.ws');
+
+    ws.addEventListener('open', (_event) => {
+        if (ws.readyState !== WebSocket.OPEN) {
+            return;
         }
-        // attempt to connect
-        socket = new WebSocket("ws://localhost:3000/chat.ws");
-    };
 
-    // Connection opened
-    socket.addEventListener("open", (event) => {
-        socket.send(
+        if (reconnected) {
+            ws.send(
+                JSON.stringify({
+                    id: crypto.randomUUID(),
+                    platform: 'ChatOverlay',
+                    username: 'ChatOverlay',
+                    message: 'ChatOverlay reconnected.',
+                    sent_at: Date.now(), // System timestamp for display ordering.
+                    received_at: Date.now(), // Local timestamp for management.
+                    avatar: 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==',
+                    is_premium: false,
+                    amount: 0,
+                    currency: 'ZWL',
+                    is_verified: false,
+                    is_sub: false,
+                    is_mod: false,
+                    is_owner: false,
+                    is_staff: false,
+                })
+            );
+            return;
+        }
+
+        ws.send(
             JSON.stringify({
                 id: crypto.randomUUID(),
-                platform: "ChatOverlay",
-                username: "ChatOverlay",
-                message: "ChatOverlay connected.",
+                platform: 'ChatOverlay',
+                username: 'ChatOverlay',
+                message: 'ChatOverlay connected.',
                 sent_at: Date.now(), // System timestamp for display ordering.
                 received_at: Date.now(), // Local timestamp for management.
-                avatar: "data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==",
+                avatar: 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==',
                 is_premium: false,
                 amount: 0,
-                currency: "ZWL",
+                currency: 'ZWL',
                 is_verified: false,
                 is_sub: false,
                 is_mod: false,
@@ -35,13 +53,12 @@
         );
     });
 
-    // Listen for messages
-    socket.addEventListener("message", (event) => {
+    ws.addEventListener('message', (event) => {
         const message = JSON.parse(event.data);
         console.log(message);
         // check if element already exists
         if (document.getElementById(message.id) === null) {
-            let el = document.createElement("div");
+            let el = document.createElement('div');
             main.appendChild(el);
             el.outerHTML = message.html;
             el.scrollIntoView();
@@ -52,13 +69,17 @@
         }
     });
 
-    socket.addEventListener("close", (event) => {
-        console.log("Socket has closed. Attempting reconnect.", event.reason);
-        setTimeout(function () { reconnect(socket); }, 3000);
+    ws.addEventListener('close', (event) => {
+        console.log('Socket connection closed. Attempting reconnect in 1 second.');
+        setTimeout(() => {
+            connect(true)
+        }, 1000);
     });
 
-    socket.addEventListener("error", (event) => {
-        socket.close();
-        setTimeout(function () { reconnect(socket); }, 3000);
+    ws.addEventListener('error', (error) => {
+        console.error('Could not connect to Stream-Nexus WebSocket server.');
+        ws.close();
     });
-})();
+}
+
+connect();
